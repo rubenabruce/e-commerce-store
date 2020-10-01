4ad8f262f99dc4f9c6b8834c1a6edf789d792dce
rubenabruce <rabruben@hotmail.co.uk>
2020-10-01 12:32:18 +0100

    Built sign in page with re-usable components form-input, custom-button and sign-in

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import Header from './components/header/header.component.jsx';
-import HomePage from './pages/homepage/homepage.component.jsx';
+import Header from './components/header/header.component';
+import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUp}/>
       </Switch>
     </div>
   );
