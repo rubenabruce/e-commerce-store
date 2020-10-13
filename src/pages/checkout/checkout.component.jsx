b3a8cd26c7265b026f4f663861ec1f52c38cfb3a
Ruben home comp <rabruben@hotmail.co.uk>
2020-10-13 15:47:06 +0100

    intergrated stripe payment functionalities, updated favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
-
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
+import StripeButton from '../../components/stripe-button/stripe-button.component';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
@@ -36,6 +37,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className='total'>
       <span>TOTAL: £{total}</span>
     </div>
+    <div className='test-warning'>
+      *Please use the following test credit card details for payments*
+      <br/>
+      Visa: 4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 )
 
